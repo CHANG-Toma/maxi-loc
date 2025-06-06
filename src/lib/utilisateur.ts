5e5eb5bd858c9b81a8e7d2476ee310591d111504
CHANG-Toma <toma11chang@gmail.com>
2025-06-06 10:22:42 +0200

    fix(utilisateur): comments

--- src/lib/utilisateur.ts
+++ src/lib/utilisateur.ts
@@ -96,7 +96,7 @@ const utilisateurSchema = z.object({
   email: z.string().email("Email invalide"),
   mot_de_passe: z
     .string()
-    .min(8, "Le mot de passe doit contenir au moins 8 caractères")
+    .min(12, "Le mot de passe doit contenir au moins 12 caractères")
     .regex(/[A-Z]/, "Le mot de passe doit contenir au moins une majuscule")
     .regex(/[a-z]/, "Le mot de passe doit contenir au moins une minuscule")
     .regex(/[0-9]/, "Le mot de passe doit contenir au moins un chiffre")
