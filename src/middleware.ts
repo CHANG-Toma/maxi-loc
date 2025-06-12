710cf19bf4256af498257829f16f53f61d9b77ef
CHANG-Toma <toma11chang@gmail.com>
2025-06-12 10:07:24 +0200

    fix(middleware): comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,8 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { validateSession } from '@/lib/session';
 
-// Routes qui ne nécessitent pas d'authentification
+// Routes qui ne nécessitent pas d'authentification (route non protégée par le middleware) 
+// #OWASP A1 (Injection) | #OWASP A6 (Sécurité des composants)
 const publicRoutes = [
   "/",
   "/login",
@@ -23,37 +24,23 @@ export async function middleware(request: NextRequest) {
 
   if (sessionToken) {
     try {
-      const user = await validateSession(sessionToken); // Si la session est valide, l'utilisateur est authentifié
-      isAuthenticated = !user; // Si la session est valide, l'utilisateur est authentifié
+      const user = await validateSession(sessionToken); // Si la session est valide, l'utilisateur est authentifié #OWASP A7 (Validation des entrées)
+      isAuthenticated = !user;
     } catch (error) {
       console.error("Erreur lors de la validation de la session:", error);
     }
   }
 
-  // Vérifier si la route actuelle est une route publique
+  // Vérifier si la route actuelle est une route publique #OWASP A1 (Injection)
   const isPublicRoute = publicRoutes.some(route => 
     request.nextUrl.pathname === route || 
     request.nextUrl.pathname.startsWith(route + "/")
   );
 
-  // Si l'utilisateur n'est pas connecté et essaie d'accéder à une route protégée
+  // Si l'utilisateur n'est pas connecté et essaie d'accéder à une route protégée | #OWASP A1 (Injection) | #OWASP A6 (Sécurité des composants)
   if (!isAuthenticated && !isPublicRoute) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
   return NextResponse.next();
 }
-
-// Configuration des routes à protéger
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
-}; 
