cf1d3ab55debb14d47b6f779d8b30ec290107aa0
CHANG-Toma <toma11chang@gmail.com>
2025-06-19 15:08:30 +0200

    Fix Prisma engine issue with PlanetScale serverless driver

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,18 @@
 import { PrismaClient } from '../../prisma/generated/client'
+import { PrismaPlanetScale } from '@prisma/adapter-planetscale'
+import { fetch as undiciFetch } from 'undici'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
+const connectionString = process.env.DATABASE_URL!
+
+// Utiliser l'adaptateur PlanetScale pour l'environnement de production
+const adapter = new PrismaPlanetScale({ 
+  url: connectionString, 
+  fetch: undiciFetch 
+})
+
 export const prisma = 
-  globalForPrisma.prisma || new PrismaClient()
+  globalForPrisma.prisma || new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
